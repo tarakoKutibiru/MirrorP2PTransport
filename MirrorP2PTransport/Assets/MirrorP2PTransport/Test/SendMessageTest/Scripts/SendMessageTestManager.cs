9bb186eb74814cb127e74616e807c95a55b9acd7
sena_kuzuoka <senakuzuoka@gmail.com>
2020-10-29 01:53:31 +0900

    wip: NetworkManagerを仲介せず直接WebRTCで送信

--- MirrorP2PTransport/Assets/MirrorP2PTransport/Test/SendMessageTest/Scripts/SendMessageTestManager.cs
+++ MirrorP2PTransport/Assets/MirrorP2PTransport/Test/SendMessageTest/Scripts/SendMessageTestManager.cs
@@ -6,15 +6,13 @@ namespace Mirror.WebRTC.Test.SendMessage
     public class SendMessageTestManager : MonoBehaviour
     {
         public InputField inputField = null;
+        public MirrorP2PTransport mirrorP2PTransport = null;
 
         public void Send()
         {
             if (this.inputField == null) return;
 
-            Message message = new Message();
-            message.message = this.inputField.text;
-
-            NetworkClient.Send(message);
+            this.mirrorP2PTransport.DebugSend(this.inputField.text);
         }
     }
 }
