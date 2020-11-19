426058c27ec58584b471af2348b0aeefd2a26cae
sena_kuzuoka <senakuzuoka@gmail.com>
2020-11-20 01:50:33 +0900

    load signaling settings from scrictable object

---
+++ MirrorP2PTransport/Assets/MirrorP2PTransport/Samples/Common/Scripts/AyameSignalingSettings.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Mirror.WebRTC.Samples
+{
+    /// <summary>
+    /// Signaling Server "Ayame" settings
+    /// </summary>
+    [CreateAssetMenu(fileName = "AyameSignalingSettings", menuName = "MirrorP2PTransport/AyameSignalingSettings")]
+    public class AyameSignalingSettings : ScriptableObject
+    {
+        public string signalingUrl;
+        public string signalingKey;
+        public string roomBaseId;
+    }
+}
---
+++ MirrorP2PTransport/Assets/MirrorP2PTransport/Samples/Common/Scripts/MirrorP2PTransport.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Mirror.WebRTC.Samples
+{
+    public class MirrorP2PTransport : Mirror.WebRTC.MirrorP2PTransport
+    {
+        protected override void Awake()
+        {
+            var settings = Resources.Load<AyameSignalingSettings>("AyameSignalingSettings");
+
+            if (settings != default)
+            {
+                if (string.IsNullOrEmpty(this.signalingKey)) this.signalingKey = settings.signalingKey;
+                if (string.IsNullOrEmpty(this.signalingURL)) this.signalingURL = settings.signalingUrl;
+                if (string.IsNullOrEmpty(this.roomId)) this.roomId = settings.roomBaseId + "_" + SceneManager.GetActiveScene().name;
+            }
+
+            base.Awake();
+        }
+    }
+}
