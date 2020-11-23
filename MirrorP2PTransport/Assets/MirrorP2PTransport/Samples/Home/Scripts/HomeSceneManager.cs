bfa4db734fc3ab59fc8dd92f775473d3cddccd24
sena_kuzuoka <senakuzuoka@gmail.com>
2020-11-23 23:29:04 +0900

    can return home scene

--- MirrorP2PTransport/Assets/MirrorP2PTransport/Samples/Home/Scripts/HomeSceneManager.cs
+++ MirrorP2PTransport/Assets/MirrorP2PTransport/Samples/Home/Scripts/HomeSceneManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,14 +7,52 @@ namespace Mirror.WebRTC.Samples
 {
     public class HomeSceneManager : MonoBehaviour
     {
+        private void Awake()
+        {
+            var find = GameObject.FindGameObjectsWithTag("HomeSceneManager");
+            if (1 < find.Length)
+            {
+                Destroy(this);
+                return;
+            }
+
+            DontDestroyOnLoad(this);
+        }
+
+        private void Start()
+        {
+            SceneManager.sceneLoaded += this.OnSceneLoaded;
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != "HomeScene") return;
+
+            var find = GameObject.FindGameObjectsWithTag("NetworkManager");
+            if (find == default) return;
+            find.ToList().ForEach(x => Destroy(x));
+        }
+
         private void OnGUI()
+        {
+            if (SceneManager.GetActiveScene().name == "HomeScene")
+            {
+                this.ShowHomeSceneGUI();
+            }
+            else
+            {
+                this.OnReturnButtonGUI();
+            }
+        }
+
+        void ShowHomeSceneGUI()
         {
             var sceneNames = new List<string>()
             {
             "SignalingTestScene",
             "SendMessageTestScene",
             "SignalingTestScene",
-            "SyncTranformTestScene",
+            "SyncTransformTestScene",
 
             "BasicScene",
             "ChatScene",
@@ -27,14 +66,14 @@ namespace Mirror.WebRTC.Samples
 
             GUILayout.BeginVertical();
             GUILayout.FlexibleSpace();
-            sceneNames.ForEach(s => this.OnButtonGUI(s));
+            sceneNames.ForEach(s => this.OnSceneButtonGUI(s));
             GUILayout.FlexibleSpace();
             GUILayout.EndVertical();
 
             GUILayout.EndArea();
         }
 
-        void OnButtonGUI(string sceneName)
+        void OnSceneButtonGUI(string sceneName)
         {
             float mingHeight = Screen.height / 20f;
             float margin = Screen.width / 100f;
@@ -44,6 +83,29 @@ namespace Mirror.WebRTC.Samples
             {
                 SceneManager.LoadScene(sceneName);
             }
+        }
+
+        void OnReturnButtonGUI()
+        {
+            GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+            GUILayout.BeginHorizontal();
+            {
+                GUILayout.FlexibleSpace();
+
+                float mingHeight = Screen.height / 20f;
+                float minWidth = Screen.width / 10f;
+
+                float margin = Screen.width / 100f;
+                GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+
+                if (GUILayout.Button("Return", buttonStyle, GUILayout.MinWidth(minWidth), GUILayout.MinHeight(mingHeight)))
+                {
+                    SceneManager.LoadScene("HomeScene");
+                }
+
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.EndArea();
         }
     }
 }
