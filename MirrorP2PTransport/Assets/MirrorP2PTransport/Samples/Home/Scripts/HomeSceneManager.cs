043b9da48964377b161497110c09ee7d5b20d6ba
sena_kuzuoka <senakuzuoka@gmail.com>
2020-11-23 22:39:07 +0900

    create Home Scene

---
+++ MirrorP2PTransport/Assets/MirrorP2PTransport/Samples/Home/Scripts/HomeSceneManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Mirror.WebRTC.Samples
+{
+    public class HomeSceneManager : MonoBehaviour
+    {
+        private void OnGUI()
+        {
+            var sceneNames = new List<string>()
+            {
+            "SignalingTestScene",
+            "SendMessageTestScene",
+            "SignalingTestScene",
+            "SyncTranformTestScene",
+
+            "BasicScene",
+            "ChatScene",
+            "PongScene",
+            "RigidbodyPhysicsScene",
+            "TanksScene",
+            };
+
+            float margin = Screen.width / 100f;
+            GUILayout.BeginArea(new Rect(margin, margin, Screen.width - margin * 2f, Screen.height - margin * 2f));
+
+            GUILayout.BeginVertical();
+            GUILayout.FlexibleSpace();
+            sceneNames.ForEach(s => this.OnButtonGUI(s));
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+
+            GUILayout.EndArea();
+        }
+
+        void OnButtonGUI(string sceneName)
+        {
+            float mingHeight = Screen.height / 20f;
+            float margin = Screen.width / 100f;
+            GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
+
+            if (GUILayout.Button(sceneName, buttonStyle, GUILayout.MinWidth(Screen.width - margin * 2f), GUILayout.MinHeight(mingHeight)))
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+}
