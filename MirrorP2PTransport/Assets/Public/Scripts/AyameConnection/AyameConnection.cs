f6d2166a1fa912807f0ea5c3c9eb5d4b1936e75b
sena_kuzuoka <senakuzuoka@gmail.com>
2021-07-17 01:34:43 +0900

    wip

---
+++ MirrorP2PTransport/Assets/Public/Scripts/AyameConnection/AyameConnection.cs
@@ -0,0 +1,34 @@
+﻿namespace Mirror.WebRTC
+{
+    public class AyameConnection
+    {
+#if UNITY_WEBGL
+        IAyameConnectionImpl impl=new AyameConnectionWebGLImpl();
+#else
+        IAyameConnectionImpl impl = new AyameConnectionImpl();
+#endif
+        public OnMessageDelegate OnMessageHandler => this.impl.OnMessageHandler;
+        public OnConnectedDelegate OnConnectedHandler { get => this.impl.OnConnectedHandler; set => this.impl.OnConnectedHandler = value; }
+        public OnDisconnectedDelegate OnDisconnectedHandler => this.impl.OnDisconnectedHandler;
+
+        public void Connect(string signalingURL, string signalingKey, string roomId, string[] dataChannelLabels, float timeOut)
+        {
+            this.impl.Connect(signalingURL, signalingKey, roomId, dataChannelLabels, timeOut);
+        }
+
+        public void Disconnect()
+        {
+            this.impl.Disconnect();
+        }
+
+        public void SendMessage(string dataChannelLabel, byte[] data)
+        {
+            this.impl.SendMessage(dataChannelLabel, data);
+        }
+
+        public bool IsConnected(string dataChannelLabel)
+        {
+            return this.impl.IsConnected(dataChannelLabel);
+        }
+    }
+}
---
+++ MirrorP2PTransport/Assets/Public/Scripts/AyameConnection/IAyameConnectionImpl.cs
@@ -0,0 +1,22 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using Unity.WebRTC;
+
+namespace Mirror.WebRTC
+{
+    public delegate void OnMessageDelegate(string dataChannelLabel, byte[] rawData);
+    public delegate void OnConnectedDelegate(RTCDataChannel dataChannel);
+    public delegate void OnDisconnectedDelegate();
+
+    interface IAyameConnectionImpl
+    {
+        OnMessageDelegate OnMessageHandler { get; set; }
+        OnConnectedDelegate OnConnectedHandler { get; set; }
+        OnDisconnectedDelegate OnDisconnectedHandler { get; set; }
+
+        UniTask<List<int>> Connect(string signalingURL, string signalingKey, string roomId, string[] dataChannelLabels, float timeOut);
+        void Disconnect();
+        void SendMessage(string dataChannelLabel, byte[] data);
+        bool IsConnected(string dataChannelLabel);
+    }
+}
